ffd5a09f2a03acf6f4be7b526a4bb509dea34e64
PeroxideWE <william.eriksson@gmail.com>
2018-03-06 11:33:27 +0100

    Working hardtyped VM

--- Crater/Crater/Controllers/IndexController.cs
+++ Crater/Crater/Controllers/IndexController.cs
@@ -26,7 +26,7 @@ namespace Crater.Controllers
         //ajax anrop till controller
         [HttpGet]
         //[Route("movie/moviebox/{idName}")]
-        public IActionResult Info(dynamic craterName)
+        public IActionResult Info(string craterNameInputParameter)
         {
             // var crater = repository.GetCraterByName(craterName);
 
@@ -37,7 +37,7 @@ namespace Crater.Controllers
                 CraterName="Dellen",
                 Diameter=23,
                 Type="Stone"
-
+                
                 // CraterName = crater.craternname;
                 //MovieName = movie.MovieName,
                 //MovieSum = movie.MovieSum
