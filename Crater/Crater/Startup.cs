73971b87337244640c5b5264186e5d5b490db353
PeroxideWE <william.eriksson@gmail.com>
2018-03-06 14:32:56 +0100

    Skapa databas + scaffolding

--- Crater/Crater/Startup.cs
+++ Crater/Crater/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Crater.Models.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Crater
@@ -15,8 +17,10 @@ namespace Crater
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Crater;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             services.AddMvc();
-
+            services.AddDbContext<CraterContext>(o => o.UseSqlServer(connectionString));
+            //Scaffold-DbContext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Crater;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" Microsoft.EntityFrameworkCore.SqlServer -OutputDir "Models/Entities" -Context "CraterContext" -Force
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
